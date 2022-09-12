f8fb5e803f4ea67a244d70803e6ad14e22b49d15
anand8435 <anand@131296@gmail.com>
2022-09-12 11:48:57 +0530

    add orsproject_0

---
+++ ORSProject_0/src/main/java/in/co/sunrays/proj0/exception/ApplicationException.java
@@ -0,0 +1,19 @@
+package in.co.sunrays.proj0.exception;
+// TODO: Auto-generated Javadoc
+
+/**
+ * @author anand
+ *
+ * The Class ApplicationException.
+ */
+public class ApplicationException extends Exception{
+
+/**
+ * Instantiates a new application exception.
+ *
+ * @param msg the msg
+ */
+public ApplicationException(String msg){
+	super(msg);
+}
+}
---
+++ ORSProject_0/src/main/java/in/co/sunrays/proj0/exception/DatabaseException.java
@@ -0,0 +1,19 @@
+package in.co.sunrays.proj0.exception;
+// TODO: Auto-generated Javadoc
+
+/**
+ * @author annad
+ *
+ * The Class DatabaseException.
+ */
+public class DatabaseException extends Exception {
+
+/**
+ * Instantiates a new database exception.
+ *
+ * @param msg the msg
+ */
+public DatabaseException(String msg){
+	super(msg);
+}
+}
